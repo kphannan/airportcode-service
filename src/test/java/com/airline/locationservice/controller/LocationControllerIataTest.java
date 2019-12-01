cb14e7db3eb5984101450cf5bdfcbfe8161dbcf2
Kevin Hannan <kphannan@gmail.com>
2019-12-01 01:30:51 -0500

    Finally a running unit test for the service; and a Integration Test

--- src/test/java/com/airline/locationservice/controller/LocationControllerIataTest.java
+++ src/test/java/com/airline/locationservice/controller/LocationControllerIataTest.java
@@ -1,4 +1,4 @@
-// package com.airline.locationservice.controller;
+package com.airline.locationservice.controller;
 
 // import static org.hamcrest.MatcherAssert.assertThat;
 // import static org.hamcrest.Matchers.both;
@@ -8,32 +8,93 @@
 // import static org.junit.jupiter.api.Assertions.assertThrows;
 // import static org.mockito.Mockito.when;
 
-// import com.airline.locationservice.repository.AirportCodeIataRepository;
-// import com.airline.core.location.AirportCode;
+import static org.assertj.core.api.Assertions.*;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.mock.mockito.MockBean;
-// import org.springframework.http.ResponseEntity;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.ArgumentMatchers.any;
+// import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.*;
+// import static org.mockito.Mockito.when;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import java.util.Optional;
+
+import com.airline.locationservice.repository.AirportCodeIata;
+import com.airline.locationservice.repository.AirportCodeIataRepository;
+import com.airline.core.location.AirportCode;
+import com.airline.core.location.IATAAirportCode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+// import org.springframework.boot.test.mock.mockito.Mock;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletResponse;
 // import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 
-// @DataJpaTest
-// public class LocationControllerIataTest
-// {
-//     // mock repository
-//     @Autowired
-//     AirportCodeIataRepository mockRepository;
+// @ExtendWith(MockitoExtension.class)
+@SpringBootTest
+public class LocationControllerIataTest
+{
+    // @Autowired
+    private MockMvc   mvc;
 
-//     @Test
-//     public void getSingleKnownIataAirportCode()
-//     {
+    // mock repository
+    @Mock
+    AirportCodeIataRepository repository;
 
-//         LocationControllerIata controller = new LocationControllerIata( mockRepository );
+    @InjectMocks
+    LocationControllerIata    service;
+    // private AirportCodeRes
 
-//         when( mockRepository.findById("ATL")).thenReturn(null);
-//         ResponseEntity<AirportCode> iataResp = controller.one( "ATL" );
-//     }
-// }
+    @BeforeEach
+    public void setup()
+    {
+        mvc = MockMvcBuilders.standaloneSetup(service)
+                // .setControllerAdvice(new SuperHeroExceptionHandler())
+                // .addFilters(new SuperHeroFilter())
+                .build();
+    }
 
+
+    @Test
+    public void getSingleKnownIataAirportCode()
+        throws Exception
+    {
+        IATAAirportCode airportCode = new IATAAirportCode( "ATL" );
+        assertThat( airportCode ).isNotNull();
+
+        // when( repository.findById("ATL")).thenReturn( Optional.of( new AirportCodeIata( airportCode.getAirportCode() )));
+        given( repository.findById("ATL")).willReturn( Optional.of( new AirportCodeIata( airportCode.getAirportCode() )));
+
+        MockHttpServletResponse response = mvc.perform(
+            get("/airport/iata/{id}", airportCode.getAirportCode() )
+                .accept( MediaType.APPLICATION_JSON ))
+            // .andExpect().ok()
+            .andReturn().getResponse();
+
+        // assertThat(response).ok();
+        // LocationControllerIata controller = new LocationControllerIata( mockRepository );
+
+        // when( mockRepository.findById("ATL")).thenReturn(null);
+        // ResponseEntity<AirportCode> iataResp = controller.one( "ATL" );
+    }
+}
+
+ 
+ 
  
