3b3e6adf9e0b361568d93a4bd117add9819df6e2
Kevin Hannan <kphannan@gmail.com>
2019-12-22 10:47:57 -0500

    Cleanup some commented - old setup code

--- src/main/java/com/airline/locationservice/LocationServiceApplication.java
+++ src/main/java/com/airline/locationservice/LocationServiceApplication.java
@@ -19,48 +19,5 @@ public class LocationServiceApplication
 		SpringApplication.run( LocationServiceApplication.class, args );
     }
     
-
-    // @Bean
-    // public CommandLineRunner demo( AirportCodeRepository repository)
-    // // public CommandLineRunner demo( AirportCodeRepository repository )
-    // {
-    //     return (args) -> {
-    //         log.info( "----- Load the DB -----" );
-    //         repository.save( new AirportCode( "ATL" ));
-    //         repository.save( new AirportCode( "MSP" ));
-    //         repository.save( new AirportCode( "NRT" ));
-
-
-    //         log.info( "Fetch items from DB");
-    //         for (AirportCode airport : repository.findAll() )
-    //         {
-    //             log.info( airport.toString() );
-    //         }
-    //         log.info( "----- Done -----");
-    //     };
-    // }
-
-
-
-    /**
-     * This is the cors filter.
-     *
-     * @return
-     */
-    // @Bean
-	// public FilterRegistrationBean corsFilter() 
-	// {
-    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    //     CorsConfiguration config = new CorsConfiguration();
-    //     config.setAllowCredentials(true);
-    //     config.addAllowedOrigin("*");
-    //     config.addAllowedHeader("*");
-    //     config.addAllowedMethod("*");
-    //     source.registerCorsConfiguration("/**", config);
-    //     FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-    //     bean.setOrder(0);
-    //     return bean;
-    // }
-
 }
 
